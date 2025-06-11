168d73e8e67953b2bb36d7a2b5cd4dc4bb4da3b0
MartinBINARD <martin.binard.dev@gmail.com>
2025-06-11 10:03:49 -1000

    Add unit tests to layer service

--- src/main/java/com/app/tennis/service/PlayerNotFoundException.java
+++ src/main/java/com/app/tennis/service/PlayerNotFoundException.java
@@ -2,6 +2,6 @@ package com.app.tennis.service;
 
 public class PlayerNotFoundException extends  RuntimeException {
     public PlayerNotFoundException(String lastName) {
-        super("Player with lastname " + lastName + " could not be found");
+        super("Player with lastname " + lastName + " could not be found.");
     }
 }
